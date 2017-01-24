7b202478c8197f5c934976037274ed94bcb43a32
Samuel Negri Morais <samukanm@gmail.com>
2017-01-24 01:30:05 -0200

    Initial commit

---
+++ src/main/java/dextra/Bank.java
@@ -0,0 +1,22 @@
+package dextra;
+
+import java.util.Date;
+
+public class Bank {
+
+    public void credit(String accountId, String description, Float amount, Date date) {
+
+    }
+
+    public void debit(String accountId, String description, Float amount, Date date) {
+
+    }
+
+    public void balance(String accountId) {
+
+    }
+
+    public void statement(String accountId) {
+
+    }
+}
---
+++ src/test/java/dextra/BankTest.java
@@ -0,0 +1,14 @@
+package dextra;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BankTest {
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
